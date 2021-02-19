186080bd443e4f0fe4188a9470795233fcca6a5f
Matthew McCall <matthewm3004@icloud.com>
2021-02-19 09:18:25 -0500

    Initial Commit

---
+++ bpapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bpapp
+//
+//  Created by Matthew McCall on 2/19/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bpapp/bpappApp.swift
@@ -0,0 +1,17 @@
+//
+//  bpappApp.swift
+//  bpapp
+//
+//  Created by Matthew McCall on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct bpappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
