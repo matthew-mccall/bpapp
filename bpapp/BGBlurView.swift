38b1bbc653c6168a86e08714fcdb66630fe28f47
Matthew McCall <matthewm3004@icloud.com>
2021-02-21 09:30:36 -0500

    Added cards

---
+++ bpapp/BGBlurView.swift
@@ -0,0 +1,20 @@
+//
+//  BGBlurView.swift
+//  bpapp
+//
+//  Created by Matthew McCall on 2/21/21.
+//
+
+import SwiftUI
+
+struct BGBlurView: UIViewRepresentable {
+    
+    var style: UIBlurEffect.Style = .systemMaterial
+    
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
+    }
+}
---
+++ bpapp/Post.swift
@@ -0,0 +1,14 @@
+//
+//  Post.swift
+//  bpapp
+//
+//  Created by Matthew McCall on 2/21/21.
+//
+
+import Foundation
+
+struct Post {
+    var username: String
+    var imgURL: String
+    var desc: String
+}
